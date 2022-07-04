796bd99ac7823692f8d20ac0135a6b378cbfb385
CSH <ssunghwan1@gmail.com>
2022-07-04 20:40:26 +0900

    first commit!!!

---
+++ src/main/java/com/skcc/onepos/OneposApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.onepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skcc/onepos/OneposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcc.onepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
